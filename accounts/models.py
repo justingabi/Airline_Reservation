9ec82f201de486b4b54f8e97e4bc09dbcbc736b9
Justinxd <justin.gabison.20@usjr.edu.ph>
2023-05-09 00:59:39 +0800

    Added values for User + Changed the settings for auth

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Add any additional fields you want, e.g.
-    # bio = models.TextField(max_length=500, blank=True)
+    userId = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
 
     def str(self):
         return self.username
